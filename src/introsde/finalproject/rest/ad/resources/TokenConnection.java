affc8cfb75c13eaf8c3b5b203b331dd518c7cf7d
Carlo Nicolò <carlo.nicolo@studenti.unitn.it>
2016-01-12 15:58:08 +0100

    small changes

--- src/introsde/finalproject/rest/ad/resources/TokenConnection.java
+++ src/introsde/finalproject/rest/ad/resources/TokenConnection.java
@@ -3,15 +3,15 @@ package introsde.finalproject.rest.ad.resources;
 public class TokenConnection {
 	
 	//Your Twitter App's Consumer Key
-    static String consumerKey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    static String consumerKey = "bbbbbbbbbbbbbbbbbbbbbbbbbbb";
 
     //Your Twitter App's Consumer Secret
-    static String consumerSecret = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+    static String consumerSecret = "ccccccccccccccccccccc";
 
     //Your Twitter Access Token
-    static String accessToken = "gggggggggggggggggggggggggggggggggggggggg";
+    static String accessToken = "dddddddddddddddddddddddddddddd";
 
     //Your Twitter Access Token Secret
-    static String accessTokenSecret = "tttttttttttttttttttttttttttttttttttttttt";
+    static String accessTokenSecret = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
 
 }
