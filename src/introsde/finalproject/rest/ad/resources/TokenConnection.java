3619d5c34c83bf3666f83defc506b664d5380b4f
Carlo Nicolò <carlo.nicolo@studenti.unitn.it>
2016-01-12 17:04:05 +0100

    TokenConnection

---
+++ src/introsde/finalproject/rest/ad/resources/TokenConnection.java
@@ -0,0 +1,17 @@
+package introsde.finalproject.rest.ad.resources;
+
+public class TokenConnection {
+	
+	//Your Twitter App's Consumer Key
+    static String consumerKey = "tFYY0NpA1kld3LnokpW5wbUIc";
+
+    //Your Twitter App's Consumer Secret
+    static String consumerSecret = "Fdkul3oTU2mgfN4Lcw3YoSXPav2XCWKI7XHDXCD5P8jr1ZxPCr";
+
+    //Your Twitter Access Token
+    static String accessToken = "386864189-b7vX8epmWOqycbj74dpKQfHTDGOe9Grgbdtt3IF2";
+
+    //Your Twitter Access Token Secret
+    static String accessTokenSecret = "Q2tdRxKvokmbhJvHVWaJy34i5vRvVErmUzOXMaeTBtZq4";
+
+}
